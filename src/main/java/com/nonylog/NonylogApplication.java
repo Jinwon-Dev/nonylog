4bd1f9d393293155648a8d0b1fc8ac75976506bc
Jinwon-Dev <yjw9424@naver.com>
2023-06-17 23:34:47 +0900

    setting files

---
+++ src/main/java/com/nonylog/NonylogApplication.java
@@ -0,0 +1,13 @@
+package com.nonylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonylogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nonylog/NonylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
