e2eb82e356e61e3f9b5ffcf616fe0177bb83016d
Jinwon-Dev <yjw9424@naver.com>
2023-06-19 21:22:31 +0900

    feat: 게시글 저장 기능 개발 및 테스트

--- src/main/java/com/nonylog/api/controller/PostController.java
+++ src/main/java/com/nonylog/api/controller/PostController.java
@@ -1,7 +1,9 @@
 package com.nonylog.api.controller;
 
 import com.nonylog.api.request.CreatePostRequest;
+import com.nonylog.api.service.PostService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,11 +11,15 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PostController {
 
-    @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid final CreatePostRequest params) {
+    private final PostService postService;
 
+    @PostMapping("/posts")
+    public Map<String, String> post(@RequestBody @Valid final CreatePostRequest request) {
+
+        postService.write(request);
         return Map.of();
     }
 }
---
+++ src/main/java/com/nonylog/api/service/PostService.java
@@ -0,0 +1,23 @@
+package com.nonylog.api.service;
+
+import com.nonylog.api.domain.Post;
+import com.nonylog.api.repository.PostRepository;
+import com.nonylog.api.request.CreatePostRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public void write(CreatePostRequest request) {
+
+        Post post = new Post(request.getTitle(), request.getContent());
+
+        postRepository.save(post);
+    }
+}
