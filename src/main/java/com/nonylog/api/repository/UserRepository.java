b56e68588c08fbdf41798152faeb702c9dfdca3b
Jinwon-Dev <yjw9424@naver.com>
2023-06-25 21:02:09 +0900

    feat: 사전 데이터 삽입 및 로그인 기능 구현

---
+++ src/main/java/com/nonylog/api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.nonylog.api.repository;
+
+import com.nonylog.api.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` (name, email, password, created_at) VALUES ('지노니', 'jinony99@gmail.com', '1234', '2023-06-25 20:44:00')
