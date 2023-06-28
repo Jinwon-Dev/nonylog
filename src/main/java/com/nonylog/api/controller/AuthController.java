f511096b059a26b39e6527ee590e5e9a798af3c8
Jinwon-Dev <yjw9424@naver.com>
2023-06-28 22:49:25 +0900

    feat: 쿠키를 통한 인증 및 검증

--- src/main/java/com/nonylog/api/controller/AuthController.java
+++ src/main/java/com/nonylog/api/controller/AuthController.java
@@ -1,13 +1,17 @@
 package com.nonylog.api.controller;
 
 import com.nonylog.api.request.Login;
-import com.nonylog.api.response.SessionResponse;
 import com.nonylog.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -17,9 +21,21 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login) {
+    public ResponseEntity<Object> login(@RequestBody Login login) {
 
         String accessToken = authService.signIn(login);
-        return new SessionResponse(accessToken);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // TODO: 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info(">>>>> cookie={}", cookie.toString());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
--- src/main/java/com/nonylog/global/config/AuthResolver.java
+++ src/main/java/com/nonylog/global/config/AuthResolver.java
@@ -4,13 +4,17 @@ import com.nonylog.api.domain.Session;
 import com.nonylog.api.repository.SessionRepository;
 import com.nonylog.global.config.data.UserSession;
 import com.nonylog.global.exception.Unauthorized;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -24,11 +28,20 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
 
-        String accessToken = webRequest.getHeader("Authorization");
-
-        if (accessToken == null || accessToken.equals("")) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (servletRequest == null) {
+            log.error("servletRequest null");
             throw new Unauthorized();
         }
+
+        Cookie[] cookies = servletRequest.getCookies();
+
+        if (cookies.length == 0) {
+            log.error("쿠키가 존재하지 않습니다.");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
 
         Session session = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(Unauthorized::new);
